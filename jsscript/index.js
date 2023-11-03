05cb4e9bda933f009b33bec77ee49f27c2a5b819
GustavoDesk <rocha.gustavo@escolar.ifrn.edu.br>
2023-11-02 21:10:26 -0300

    fiz uma parte do js, mas não vou abri o PR

--- jsscript/index.js
+++ jsscript/index.js
@@ -1,1 +1,9 @@
-const date = new Date();
+const request = new Request("http://localhost:3000/modelos/");
+
+const URL = request.url;
+ 
+const headers = {
+    'Content-Type':'text/x'
+}
+
+console.log(URL)
